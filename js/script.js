24e0c41c51bbcbd3d8e62115b2f0bdf1bd5e0a3a
Cecilia Cai <ceciliacai@maimai.fios-router.home>
2022-04-26 12:37:58 -0700

    delete unused page

--- js/script.js
+++ js/script.js
@@ -36,25 +36,21 @@ function closeWallet() {
     wallet.hidden = true;
     body.style.overflow = 'scroll';
 }
-function buyTicket(API_URL, PRIVATE_KEY, PUBLIC_KEY, CONTRACT_ADDRESS, USER_ADDRESS) {
-    const { ethers } = require("ethers");
-    const contract = require("../token/contracts/Ticket.sol");
-    const provider = new ethers.providers.JsonRpcProvider(API_URL);
-    const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+// function buyTicket(API_URL, PRIVATE_KEY, PUBLIC_KEY, CONTRACT_ADDRESS, USER_ADDRESS) {
+//     const { ethers } = require("ethers");
+//     const contract = require("../token/contracts/Ticket.sol");
+//     const provider = new ethers.providers.JsonRpcProvider(API_URL);
+//     const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 
-    const gasPrice = await provider.getGasPrice();
-    const nftContract = new ethers.Contract(
-        CONTRACT_ADDRESS,
-        contract.abi,
-        wallet
-    );
+//     const gasPrice = await provider.getGasPrice();
+//     const nftContract = new ethers.Contract(
+//         CONTRACT_ADDRESS,
+//         contract.abi,
+//         wallet
+//     );
 
-    const gasLimit = await nftContract.estimateGas["safeTransferFrom(address,address,uint256)"](PUBLIC_KEY, USER_ADDRESS, tokenId, { gasPrice });
-    const transaction = await nftContract["safeTransferFrom(address,address,uint256)"](PUBLIC_KEY, USER_ADDRESS, tokenId, { gasLimit });
-    await transaction.wait();
-    // console.log("Transaction Hash: ", transaction.hash);
-}
-
-// Arkane.createArkaneProviderEngine({clientId: 'Arketype'}).then(provider => {
-//     web3 = new Web3(provider);
-// });
+//     const gasLimit = await nftContract.estimateGas["safeTransferFrom(address,address,uint256)"](PUBLIC_KEY, USER_ADDRESS, tokenId, { gasPrice });
+//     const transaction = await nftContract["safeTransferFrom(address,address,uint256)"](PUBLIC_KEY, USER_ADDRESS, tokenId, { gasLimit });
+//     await transaction.wait();
+//     // console.log("Transaction Hash: ", transaction.hash);
+// }
