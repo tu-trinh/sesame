4b476da39b0e841f8c31ec6c77c3e0bee530f96f
Cecilia Cai <ceciliacai@maimai.local>
2022-04-26 16:10:27 -0700

    last edits

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ let aicon = document.getElementById('aicon');
 let connectBtn = document.getElementById('connectBtn');
 let wallet = document.getElementById('wallet-pop-up');
 let closeModal = document.getElementsByClassName('close')[0];
+let qrcode = document.getElementById("qrCode")
+let qrCodeCard = document.getElementById("qrCodeCard");
 
 aicon.addEventListener('click', goHome);
 connectBtn.addEventListener('click', openWallet);
@@ -17,13 +19,11 @@ function search() {
 }
 
 function openQRCode() {
-    var qrcode = document.getElementById("qrCode")
     if (qrcode.style.display === "none") {
         qrcode.style.display = "block";
     } else {
         qrcode.style.display = "none";
     }
-    var qrCodeCard = document.getElementById("qrCodeCard")
     qrCodeCard.style.height = "30rem";
 }
 
